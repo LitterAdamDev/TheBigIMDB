d0a742c7325a8f030cf98dfde5198d9be8d3a9c8
LitterAdamDev <adamlitter99@gmail.com>
2022-01-21 01:10:28 +0100

    fixed empty file and encoding error

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, send_file, session
 from models import MovieManager
 from datetime import timedelta
+import os
+import csv
 
 app = Flask(__name__)
 app.secret_key = "dzfJyr6ZcyGjRA7DozpPGEQjGcvDrL4P"
-app.permanent_session_lifetime = timedelta(hours=5)
+app.permanent_session_lifetime = timedelta(hours=6)
 
 manager = MovieManager("https://www.imdb.com/chart/top/")
 
@@ -19,12 +21,23 @@ def index():
         jsonfied_list = [movie.__dict__ for movie in movie_list]
         session.permanent = True
         session["movie_list"] = jsonfied_list
-        
     return render_template("index.html",items=session["movie_list"] if "movie_list" in session else [])
 
 @app.route("/download", methods=["GET","POST"])
 def download():
-    manager.generate_csv()
+    os.makedirs("outputs/", exist_ok=True)
+    with open("outputs/ratings.csv", "w", encoding="utf-16", newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter="\t")
+        csv_writer.writerow(["Place", "Title", "Rating [adjusted]", "Rating [original]", "Oscar Calculator", "Review Penalizer"])
+        list_to_return = []
+        if session["movie_list"]:
+            list_to_return = session["movie_list"]
+        else:
+            list_to_return = manager.get_list()
+
+        for idx, movie in enumerate(list_to_return,start=1):
+            csv_writer.writerow([idx, movie["title"], movie["rating_value"], movie["original_rating_value"], "+"+str(movie["bonus_value"]), "-"+str(movie["minus_value"])])
+    
     return send_file(
         "outputs/ratings.csv",
         mimetype="Content-Type: text/csv; charset=Shift_JIS",
--- models.py
+++ models.py
@@ -1,39 +1,37 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 from math import floor
-import csv
-import os
 
 class Movie():
     def __init__(self, place, rating_value, rating_counter, oscars, title):
-        self._place = place
-        self._rating_value = rating_value
-        self._original_rating_value = rating_value
-        self._rating_counter = rating_counter
-        self._oscars = oscars
-        self._title = title
-        self._bonus_value = 0
-        self._minus_value = 0
+        self.place = place
+        self.rating_value = rating_value
+        self.original_rating_value = rating_value
+        self.rating_counter = rating_counter
+        self.oscars = oscars
+        self.title = title
+        self.bonus_value = 0
+        self.minus_value = 0
 
 
 class MovieManager():
     def __init__(self,url):
-        self._url = url
-        self._list = []
-        self._highest_counter = 0
+        self.url = url
+        self.list = []
+        self.highest_counter = 0
         
     def get_list(self):
-        self._list.sort(key=lambda x: x._rating_value, reverse=True)
-        return self._list
+        self.list.sort(key=lambda x: x.rating_value, reverse=True)
+        return self.list
         
     def preprocess(self):
-        self._list = []
-        client = urlopen(self._url)
+        self.list = []
+        client = urlopen(self.url)
         page_soup = soup(client.read(), "lxml")
         client.close()
         
         main_container = page_soup.find("tbody",{"class" : "lister-list"})
-        sub_containers = main_container.find_all("tr",limit=20)
+        sub_containers = main_container.find_all("tr",limit=2)
         for container in sub_containers:
             
             oscars = 0
@@ -49,10 +47,10 @@ class MovieManager():
             
             rating_counter = container.find("span",{"name" : "nv"}).attrs["data-value"]
             
-            if int(rating_counter) > int(self._highest_counter):
-                self._highest_counter = int(rating_counter)
+            if int(rating_counter) > int(self.highest_counter):
+                self.highest_counter = int(rating_counter)
             
-            self._list.append(
+            self.list.append(
                 Movie(
                     place=container.find("span",{"name" : "rk"}).attrs["data-value"],
                     rating_value=round(float(container.find("span",{"name" : "ir"}).attrs["data-value"]),1),
@@ -63,34 +61,24 @@ class MovieManager():
             )
     
     def review_penalizer(self):
-        for movie in self._list:
-            if movie._rating_counter < self._highest_counter:
-                movie._minus_value = round(floor((self._highest_counter - movie._rating_counter)/100000) * 0.1,1)
-                movie._rating_value = movie._rating_value - floor((self._highest_counter - movie._rating_counter)/100000) * 0.1
+        for movie in self.list:
+            if movie.rating_counter < self.highest_counter:
+                movie.minus_value = round(floor((self.highest_counter - movie.rating_counter)/100000) * 0.1,1)
+                movie.rating_value = movie.rating_value - floor((self.highest_counter - movie.rating_counter)/100000) * 0.1
 
     
     def oscar_calculator(self):
-        for movie in self._list:
+        for movie in self.list:
             points = 0     
             
-            if movie._oscars >=10:
+            if movie.oscars >=10:
                 points = 1.5
-            elif movie._oscars >= 6:
+            elif movie.oscars >= 6:
                 points = 1
-            elif movie._oscars >= 3:
+            elif movie.oscars >= 3:
                 points = 0.5 
-            elif movie._oscars > 0:
+            elif movie.oscars > 0:
                 points = 0.3
             
-            movie._bonus_value = points
-            movie._rating_value = round(max(min(movie._rating_value + points,10),0),1)
-            
-    def generate_csv(self):
-        list_to_return = self.get_list()
-        os.makedirs("outputs/", exist_ok=True)
-        with open("outputs/ratings.csv", "w", newline="") as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=",")
-            csv_writer.writerow(["Place","Title","Rating [adjusted]","Rating [original]","Oscar Calculator","Review Penalizer"])
-            for idx, movie in enumerate(list_to_return,start=1):
-                csv_writer.writerow([idx, movie._title, movie._rating_value, movie._original_rating_value, "+"+str(movie._bonus_value), "-"+str(movie._minus_value)])
-            
+            movie.bonus_value = points
+            movie.rating_value = round(max(min(movie.rating_value + points,10),0),1)
